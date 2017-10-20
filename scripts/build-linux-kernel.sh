aad55725c7fe37bf389b6fae337708f16b1c04dd
ikwzm <ichiro_k@ca2.so-net.ne.jp>
2017-10-20 23:12:24 +0900

    [update] documents

--- scripts/build-linux-kernel.sh
+++ scripts/build-linux-kernel.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 CURRENT_DIR=`pwd`
-LINUX_BUILD_DIR=linux-4.12.13-armv7-fpga
+LINUX_BUILD_DIR=linux-4.12.14-armv7-fpga
 
 ### Download Linux Kernel Source
 git clone git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git $LINUX_BUILD_DIR
 cd $LINUX_BUILD_DIR
-git checkout -b linux-4.12.13-armv7-fpga refs/tags/v4.12.13
+git checkout -b linux-4.12.14-armv7-fpga refs/tags/v4.12.14
 
 ### Patch for armv7-fpga
-patch -p0 < ../files/linux-4.12.13-armv7-fpga.diff
+patch -p0 < ../files/linux-4.12.14-armv7-fpga.diff
 git add --update
 git add arch/arm/configs/armv7_fpga_defconfig
 git add arch/arm/boot/dts/zynq-pynqz1.dts
 git commit -m "patch for armv7-fpga"
-git tag -a v4.12.13-armv7-fpga -m "relase v4.12.13-armv7-fpga"
+git tag -a v4.12.14-armv7-fpga -m "relase v4.12.14-armv7-fpga"
 
 ### Setup for Build 
 export ARCH=arm
@@ -29,18 +29,18 @@ make zynq-pynqz1.dtb
 make socfpga_cyclone5_de0_sockit.dtb
  
 ### Copy zImage and devicetree to tareget/zybo-zynq/boot/
-cp arch/arm/boot/zImage            ../target/zynq-zybo/boot/zImage-4.12.13-armv7-fpga
-cp arch/arm/boot/dts/zynq-zybo.dtb ../target/zynq-zybo/boot/devicetree-4.12.13-zynq-zybo.dtb
-./scripts/dtc/dtc -I dtb -O dts --symbols -o ../target/zynq-zybo/boot/devicetree-4.12.13-zynq-zybo.dts arch/arm/boot/dts/zynq-zybo.dtb
+cp arch/arm/boot/zImage            ../target/zynq-zybo/boot/zImage-4.12.14-armv7-fpga
+cp arch/arm/boot/dts/zynq-zybo.dtb ../target/zynq-zybo/boot/devicetree-4.12.14-zynq-zybo.dtb
+./scripts/dtc/dtc -I dtb -O dts --symbols -o ../target/zynq-zybo/boot/devicetree-4.12.14-zynq-zybo.dts arch/arm/boot/dts/zynq-zybo.dtb
 
 ### Copy zImage and devicetree to tareget/zybo-pynqz1/boot/
-cp arch/arm/boot/zImage              ../target/zynq-pynqz1/boot/zImage-4.12.13-armv7-fpga
-cp arch/arm/boot/dts/zynq-pynqz1.dtb ../target/zynq-pynqz1/boot/devicetree-4.12.13-zynq-pynqz1.dtb
-./scripts/dtc/dtc -I dtb -O dts --symbols -o ../target/zynq-pynqz1/boot/devicetree-4.12.13-zynq-pynqz1.dts arch/arm/boot/dts/zynq-pynqz1.dtb
+cp arch/arm/boot/zImage              ../target/zynq-pynqz1/boot/zImage-4.12.14-armv7-fpga
+cp arch/arm/boot/dts/zynq-pynqz1.dtb ../target/zynq-pynqz1/boot/devicetree-4.12.14-zynq-pynqz1.dtb
+./scripts/dtc/dtc -I dtb -O dts --symbols -o ../target/zynq-pynqz1/boot/devicetree-4.12.14-zynq-pynqz1.dts arch/arm/boot/dts/zynq-pynqz1.dtb
 
 ### Copy zImage and devicetree to tareget/de0-nano-soc/boot/
-cp arch/arm/boot/zImage                              ../target/de0-nano-soc/boot/zImage-4.12.13-armv7-fpga
-cp arch/arm/boot/dts/socfpga_cyclone5_de0_sockit.dtb ../target/de0-nano-soc/boot/devicetree-4.12.13-socfpga.dtb
-./scripts/dtc/dtc -I dtb -O dts --symbols -o ../target/de0-nano-soc/boot/devicetree-4.12.13-socfpga.dts arch/arm/boot/dts/socfpga_cyclone5_de0_sockit.dtb
+cp arch/arm/boot/zImage                              ../target/de0-nano-soc/boot/zImage-4.12.14-armv7-fpga
+cp arch/arm/boot/dts/socfpga_cyclone5_de0_sockit.dtb ../target/de0-nano-soc/boot/devicetree-4.12.14-socfpga.dtb
+./scripts/dtc/dtc -I dtb -O dts --symbols -o ../target/de0-nano-soc/boot/devicetree-4.12.14-socfpga.dts arch/arm/boot/dts/socfpga_cyclone5_de0_sockit.dtb
 
 cd ..
