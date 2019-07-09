da518f9daf1473bf8eacfec1895941c539f739fb
ikwzm <ichiro_k@ca2.so-net.ne.jp>
2019-07-10 08:11:17 +0900

    [update] scripts and documents for build linux-kernel-4.19.57-armv7-fpga

--- scripts/build-linux-kernel.sh
+++ scripts/build-linux-kernel.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
 CURRENT_DIR=`pwd`
-LINUX_BUILD_DIR=linux-4.19.55-armv7-fpga
+LINUX_BUILD_DIR=linux-4.19.57-armv7-fpga
 
 ### Download Linux Kernel Source
-git clone --depth 1 -b v4.19.55 git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git $LINUX_BUILD_DIR
+git clone --depth 1 -b v4.19.57 git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git $LINUX_BUILD_DIR
 cd $LINUX_BUILD_DIR
-git checkout -b linux-4.19.55-armv7-fpga refs/tags/v4.19.55
+git checkout -b linux-4.19.57-armv7-fpga refs/tags/v4.19.57
 
 ### Patch for armv7-fpga
-patch -p1 < ../files/linux-4.19.55-armv7-fpga.diff
+patch -p1 < ../files/linux-4.19.57-armv7-fpga.diff
 git add --update
 git add arch/arm/configs/armv7_fpga_defconfig
 git add arch/arm/boot/dts/zynq-pynqz1.dts
 git commit -m "patch for armv7-fpga"
 
 ### Patch for usb chipidea driver
-patch -p1 < ../files/linux-4.19.55-armv7-fpga-patch-usb-chipidea.diff
+patch -p1 < ../files/linux-4.19.57-armv7-fpga-patch-usb-chipidea.diff
 git add --update
 git commit -m "patch for usb chipidea driver for issue #3"
 
 ### Patch for build debian package script
 
-patch -p1 < ../files/linux-4.19.55-armv7-fpga-patch-builddeb.diff
+patch -p1 < ../files/linux-4.19.57-armv7-fpga-patch-builddeb.diff
 git add --update
 git commit -m "patch for scripts/package/builddeb to add tools/include and postinst script to header package"
 
 ### Create tag and .version
-git tag -a v4.19.55-armv7-fpga -m "release v4.19.55-armv7-fpga"
+git tag -a v4.19.57-armv7-fpga -m "release v4.19.57-armv7-fpga"
 echo 0 > .version
 
 ### Setup for Build 
@@ -44,28 +44,28 @@ make zynq-pynqz1.dtb
 make socfpga_cyclone5_de0_sockit.dtb
  
 ### Copy zImage and devicetree to tareget/zybo-zynq/boot/
-cp arch/arm/boot/zImage            ../target/zynq-zybo/boot/vmlinuz-4.19.55-armv7-fpga
-cp arch/arm/boot/dts/zynq-zybo.dtb ../target/zynq-zybo/boot/devicetree-4.19.55-zynq-zybo.dtb
-./scripts/dtc/dtc -I dtb -O dts --symbols -o ../target/zynq-zybo/boot/devicetree-4.19.55-zynq-zybo.dts arch/arm/boot/dts/zynq-zybo.dtb
+cp arch/arm/boot/zImage            ../target/zynq-zybo/boot/vmlinuz-4.19.57-armv7-fpga
+cp arch/arm/boot/dts/zynq-zybo.dtb ../target/zynq-zybo/boot/devicetree-4.19.57-zynq-zybo.dtb
+./scripts/dtc/dtc -I dtb -O dts --symbols -o ../target/zynq-zybo/boot/devicetree-4.19.57-zynq-zybo.dts arch/arm/boot/dts/zynq-zybo.dtb
 
 ### Copy zImage and devicetree to tareget/zybo-zynq-z7/boot/
-cp arch/arm/boot/zImage               ../target/zynq-zybo-z7/boot/vmlinuz-4.19.55-armv7-fpga
-cp arch/arm/boot/dts/zynq-zybo-z7.dtb ../target/zynq-zybo-z7/boot/devicetree-4.19.55-zynq-zybo-z7.dtb
-./scripts/dtc/dtc -I dtb -O dts --symbols -o ../target/zynq-zybo-z7/boot/devicetree-4.19.55-zynq-zybo-z7.dts arch/arm/boot/dts/zynq-zybo-z7.dtb
+cp arch/arm/boot/zImage               ../target/zynq-zybo-z7/boot/vmlinuz-4.19.57-armv7-fpga
+cp arch/arm/boot/dts/zynq-zybo-z7.dtb ../target/zynq-zybo-z7/boot/devicetree-4.19.57-zynq-zybo-z7.dtb
+./scripts/dtc/dtc -I dtb -O dts --symbols -o ../target/zynq-zybo-z7/boot/devicetree-4.19.57-zynq-zybo-z7.dts arch/arm/boot/dts/zynq-zybo-z7.dtb
 
 ### Copy zImage and devicetree to tareget/zybo-pynqz1/boot/
-cp arch/arm/boot/zImage              ../target/zynq-pynqz1/boot/vmlinuz-4.19.55-armv7-fpga
-cp arch/arm/boot/dts/zynq-pynqz1.dtb ../target/zynq-pynqz1/boot/devicetree-4.19.55-zynq-pynqz1.dtb
-./scripts/dtc/dtc -I dtb -O dts --symbols -o ../target/zynq-pynqz1/boot/devicetree-4.19.55-zynq-pynqz1.dts arch/arm/boot/dts/zynq-pynqz1.dtb
+cp arch/arm/boot/zImage              ../target/zynq-pynqz1/boot/vmlinuz-4.19.57-armv7-fpga
+cp arch/arm/boot/dts/zynq-pynqz1.dtb ../target/zynq-pynqz1/boot/devicetree-4.19.57-zynq-pynqz1.dtb
+./scripts/dtc/dtc -I dtb -O dts --symbols -o ../target/zynq-pynqz1/boot/devicetree-4.19.57-zynq-pynqz1.dts arch/arm/boot/dts/zynq-pynqz1.dtb
 
 ### Copy zImage and devicetree to tareget/de0-nano-soc/boot/
-cp arch/arm/boot/zImage                              ../target/de0-nano-soc/boot/vmlinuz-4.19.55-armv7-fpga
-cp arch/arm/boot/dts/socfpga_cyclone5_de0_sockit.dtb ../target/de0-nano-soc/boot/devicetree-4.19.55-socfpga.dtb
-./scripts/dtc/dtc -I dtb -O dts --symbols -o ../target/de0-nano-soc/boot/devicetree-4.19.55-socfpga.dts arch/arm/boot/dts/socfpga_cyclone5_de0_sockit.dtb
+cp arch/arm/boot/zImage                              ../target/de0-nano-soc/boot/vmlinuz-4.19.57-armv7-fpga
+cp arch/arm/boot/dts/socfpga_cyclone5_de0_sockit.dtb ../target/de0-nano-soc/boot/devicetree-4.19.57-socfpga.dtb
+./scripts/dtc/dtc -I dtb -O dts --symbols -o ../target/de0-nano-soc/boot/devicetree-4.19.57-socfpga.dts arch/arm/boot/dts/socfpga_cyclone5_de0_sockit.dtb
 
 ### Copy zImage and devicetree to tareget/de10-nano/boot/
-cp arch/arm/boot/zImage                              ../target/de10-nano/boot/vmlinuz-4.19.55-armv7-fpga
-cp arch/arm/boot/dts/socfpga_cyclone5_de0_sockit.dtb ../target/de10-nano/boot/devicetree-4.19.55-socfpga.dtb
-./scripts/dtc/dtc -I dtb -O dts --symbols -o ../target/de10-nano/boot/devicetree-4.19.55-socfpga.dts arch/arm/boot/dts/socfpga_cyclone5_de0_sockit.dtb
+cp arch/arm/boot/zImage                              ../target/de10-nano/boot/vmlinuz-4.19.57-armv7-fpga
+cp arch/arm/boot/dts/socfpga_cyclone5_de0_sockit.dtb ../target/de10-nano/boot/devicetree-4.19.57-socfpga.dtb
+./scripts/dtc/dtc -I dtb -O dts --symbols -o ../target/de10-nano/boot/devicetree-4.19.57-socfpga.dts arch/arm/boot/dts/socfpga_cyclone5_de0_sockit.dtb
 
 cd ..
